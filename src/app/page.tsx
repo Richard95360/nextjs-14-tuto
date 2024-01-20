88f7709a697e569d9c530dbc45745030d1a341b9
Votre Nom <Vous@exemple.com>
2024-01-20 18:23:11 +0100

    first1 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
         <h1 className="text-xl">Home Page</h1>
         <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
         <Link href={"/about"} className="flex items-start flex-wrap m-1 text-xl  text-slate-50">About</Link>
+        <Link href={"/photo-feed"} className="flex flex-wrap m-1 text-xl  text-slate-50">Photos</Link>
         <Link href={"/profile"} className="flex flex-wrap m-1 text-xl  text-slate-50">Profile</Link>
         <Link href={"/products"} className="flex flex-wrap m-1 text-xl  text-slate-50">Products</Link>
         <Link href={"/blog"} className="flex flex-auto text-xl  text-slate-50">Blog</Link>
-        <Link href={"/photo-feed"} className="flex flex-auto text-xl  text-slate-50">Photos</Link>
         </nav>
     </main>
   )
--- src/app/photo-feed/page.tsx
+++ src/app/photo-feed/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <h1 className="text-center text-3xl font-bold my-4">
         New Wonders of the World
       </h1>
+      <Link href={"/"}>Home</Link>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
         {wonders.map(({ id, src, name }) => (
           <Link key={id} href={`/photo-feed/${id}`}>
